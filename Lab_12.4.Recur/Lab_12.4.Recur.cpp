e5fbfec443dfef54d268c8f33f8ad7754e119d91
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2023-04-14 10:55:18 +0300

    Add project files.

---
+++ Lab_12.4.Recur/Lab_12.4.Recur.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+typedef int Info;
+struct Elem
+{
+	Elem* link;
+	Info info;
+};
+
+void Insert(Elem*& L, Elem* T, Info value)
+{
+	Elem* tmp = new Elem;
+	tmp->info = value;
+	if (L != NULL)
+	{
+		if (T->link != L)
+			Insert(L, T->link, value);
+		else
+			T->link = tmp;
+	}
+	else
+		L = tmp;
+	tmp->link = L;
+}
+
+int Count(Elem* L, Elem* first, int k)
+{
+	if (L->link == first)
+	{
+		if (L->info < 0)
+			return k + 1;
+		else
+			return k;
+	}
+	else
+	{
+		if (L->info < 0)
+			return Count(L->link, first, k + 1);
+		else
+			return Count(L->link, first, k);
+	}
+}
+
+
+void Print(Elem* L)
+{
+	if (L == NULL)
+		return;
+	static Elem* first = L;
+	cout << setw(4) << L->info;
+	if (L->link != first)
+		Print(L->link);
+	else
+		cout << endl;
+}
+
+int main()
+{
+	Elem* L = NULL;
+	Insert(L, L, -1);
+	Insert(L, L, 2);
+	Insert(L, L, 1);
+	Insert(L, L, -4);
+	Insert(L, L, 3);
+	Insert(L, L, -7);
+	
+	cout << "given list: " << endl;
+	Print(L);
+	cout << endl;
+	cout << "Count of number with negative values: ";
+	cout << Count(L, L, 0) << endl;
+	
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_12.4.Recur/Lab_12.4.Recur.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+	
+		{				
+			Elem* L = NULL;
+			Insert(L, L, -1);
+			Insert(L, L, 2);
+			Insert(L, L, 1);
+			Insert(L, L, -4);
+			Insert(L, L, 3);
+			Insert(L, L, -7);
+			
+
+			int t = Count(L, L, 0);
+			Assert::AreEqual(t, 3);
+		}
+
+		
+	};
+}
